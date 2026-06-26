45f3924f7900202e2679214d0c3f98c2997f28cf
yahy8918-cpu <yahy8918@icloud.com>
2026-06-26 12:31:17 +0300

    Add Jest configuration

---
+++ jest.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  roots: ['<rootDir>/src'],
+  testMatch: ['**/__tests__/**/*.test.ts'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  collectCoverageFrom: [
+    'src/**/*.ts',
+    '!src/**/*.d.ts',
+    '!src/index.ts',
+    '!src/gemini.ts'
+  ]
+};
